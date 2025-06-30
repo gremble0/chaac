5a15364f57db0f6a3c1fbcdf3d13563133c35118
gremble0 <hermanstornes@gmail.com>
2025-07-01 00:44:57 +0200

    Add Catch2 for tests

--- src/main.cc
+++ src/main.cc
@@ -5,7 +5,7 @@
 int main(int /*argc*/, const char *const * /*argv*/) {
     std::println("Chaac - a chess engine");
 
-    chess_board board;
+    ch::chess_board board;
     board.print();
     // board.move(0x1UL << (7U * 8 + 3), 0x1UL << (4U * 8 + 3), player_type::WHITE);
     // board.print();
---
+++ test/test.cc
@@ -0,0 +1,6 @@
+#include <catch2/catch_all.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Test moving chess pieces") {
+    SECTION("Test moving pawns") { REQUIRE(false); }
+}
